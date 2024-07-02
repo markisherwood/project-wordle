f42a9355e018e72e6ea14f56f09d7f84cdaa9483
Mark Isherwood <mark@markisherwood.me>
2024-07-02 18:28:47 +1000

    Added guess grid

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { range } from "../../utils";
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 function GuessResults({ guesses }) {
   return (
-    <div class="guess-results">
-      {guesses.map((guess, index) => {
-        return <p className="guess" key={index}>{guess}</p>;
+    <div className="guess-results">
+      {range(NUM_OF_GUESSES_ALLOWED).map((rowIndex) => {
+        return (
+          <p className="guess" key={rowIndex}>
+            {range(5).map((charIndex) => (
+              <span className="cell" key={charIndex}>{guesses?.[rowIndex]?.[charIndex]}</span>
+            ))}
+          </p>
+        );
       })}
     </div>
   );
